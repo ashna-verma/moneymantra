da09e1dd0aaa959f5f9b7bf8475a276e65c3b12e
ashna-verma <ashnaverma1706@gmail.com>
2026-03-02 23:05:25 +0530

    Email Config for Profile activation

--- src/main/java/in/ashna/moneymantra/repository/ProfileRepository.java
+++ src/main/java/in/ashna/moneymantra/repository/ProfileRepository.java
@@ -7,6 +7,10 @@ import java.util.Optional;
 
 public interface ProfileRepository extends JpaRepository <ProfileEntity, Long>{
 
+    //select * from tbl_profiles where email = ?
     Optional<ProfileRepository> findByEmail(String email);
+
+    //select * from tbl_profiles where activation_token = ?
+    Optional<ProfileEntity> findByActivationToken(String activationToken);
 
 }
--- src/main/java/in/ashna/moneymantra/service/ProfileService.java
+++ src/main/java/in/ashna/moneymantra/service/ProfileService.java
@@ -13,11 +13,19 @@ import java.util.UUID;
 public class ProfileService {
 
     private final ProfileRepository profileRepository;
+    private final EmailService emailService;
 
     public ProfileDTO registerProfile(ProfileDTO profileDTO){
         ProfileEntity newProfile= toEntity(profileDTO);
         newProfile.setActivationToken(UUID.randomUUID().toString());
         newProfile= profileRepository.save(newProfile);
+
+        //send Activation mail
+        String activationLink= "http://localhost:8080/api/v1.0/activate?token=" + newProfile.getActivationToken();
+        String subject= "Activate your MoneyMantra account";
+        String body= "Click on the following link to activate your account: "+ activationLink;
+        emailService.sendEmail(newProfile.getEmail(), subject, body);
+
         return toDTO(newProfile);
     }
 
@@ -43,6 +51,16 @@ public class ProfileService {
                 .createdAt(profileEntity.getCreatedAt())
                 .updatedAt(profileEntity.getUpdatedAt())
                 .build();
+    }
+
+    public boolean activateProfile(String activationToken){
+        return profileRepository.findByActivationToken(activationToken)
+                .map(profile ->{
+                    profile.setIsActive(true);
+                    profileRepository.save(profile);
+                    return true;
+                })
+                .orElse(false);
     }
 
 }
