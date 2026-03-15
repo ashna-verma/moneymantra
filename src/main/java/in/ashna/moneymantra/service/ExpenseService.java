c9b404c99fd0eb6104f6c9665eaff8c3d746d121
ashna-verma <ashnaverma1706@gmail.com>
2026-03-15 17:01:17 +0530

    Notification API config- reminder and reports

--- src/main/java/in/ashna/moneymantra/service/ExpenseService.java
+++ src/main/java/in/ashna/moneymantra/service/ExpenseService.java
@@ -74,6 +74,12 @@ public class ExpenseService {
         return list.stream().map(this::toDTO).toList();
     }
 
+    //Notifications
+    public List<ExpenseDTO> getExpensesForUserOnDate(Long profileId, LocalDate date) {
+        List<ExpenseEntity> list = expenseRepository.findByProfileIdAndDate(profileId, date);
+        return list.stream().map(this::toDTO).toList();
+    }
+
     //Helper methods
     private ExpenseEntity toEntity(ExpenseDTO expenseDTO, ProfileEntity profile, CategoryEntity category) {
         return in.ashna.moneymantra.entity.ExpenseEntity.builder()
